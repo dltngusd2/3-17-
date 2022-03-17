d36fdaa7e69b04e787686803a69dee0ad27a8557
이수현 <dltngusd2@naver.com>
2022-03-17 23:15:45 +0900

    집에서 한거임

---
+++ src/day02/EX05_home.java
@@ -0,0 +1,24 @@
+package day02;
+
+import java.util.Scanner;
+
+public class EX05_home {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	String bloodtype;
+	
+	System.out.println("당신의 혈액형은 무엇입니까?");
+	bloodtype = input.next();
+	System.out.println("나이는요?");
+	int age;
+	age = input.nextInt();	
+	System.out.println("반갑습니다.");
+	String hi;
+	hi = input.next();
+	System.out.println("만반잘부");
+	
+	
+	
+	
+}
+}
